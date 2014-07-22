b5c99f8f8e9054564269d0b17f52db5a3c097a78
xieQin <xieqin1991@gmail.com>
2014-07-22 12:31:19 +0800

    modify ex3 and add style.js

--- style.js
+++ style.js
@@ -2,14 +2,34 @@
  *
  * @authors Your Name (you@example.org)
  * @date    2014-07-22 09:58:59
- * @version 1.0.1
+ * @version 1.0.2
  */
+
 $(document).ready(function($) {
+	var tabs = $(".tab");
     var tabMenus = $(".tab-menuWrapper").find("li");
     var tabContents = $(".tab-contentWrapper").find("div");
     tabMenus.bind('click',function() {
+    	var tabsThis = $(this).parent("ul").parent("div");
     	var num = $(this).index();
-    	tabContents.addClass('displaynone');
-    	tabContents.eq(num).removeClass('displaynone');
+    	tabsThis.find(".tab-contentWrapper").find("div").addClass('displaynone');
+    	tabsThis.find(".tab-contentWrapper").find("div").eq(num).removeClass('displaynone');
+    })
+    tabMenus.hover(function() {
+    	var num = $(this).index();
+    	if(2 === num) {
+    		$(this).addClass('hoverFirst');
+    	}
+    	else {
+    		$(this).addClass('hoverSecond');
+    	}
+    }, function() {
+    	var num = $(this).index();
+    	if(2 === num) {
+    		$(this).removeClass('hoverFirst');
+    	}
+    	else {
+    		$(this).removeClass('hoverSecond');
+    	}
     })
 })
